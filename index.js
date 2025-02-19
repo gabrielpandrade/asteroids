3ce97ddae964efe661b38709a2770b908e2d8f96
Gabriel Pinto Andrade <pintoandradegabriel@gmail.com>
2025-02-19 20:40:38 -0300

    add: tiros adicionados

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 class Player {
-    constructor({position, velocity, size}) {
+    constructor({position, velocity}) {
         this.position = position // {x, y}
         this.velocity = velocity // {vx, vy}
         this.rotation = 0
@@ -26,13 +26,35 @@ class Player {
         ctx.lineTo(this.position.x - 10, this.position.y + 10)
         ctx.closePath()
 
-        ctx.strokeStyle = 'green'
+        ctx.strokeStyle = 'white'
         ctx.stroke()
 
         ctx.restore()
     }
 
     update() {
+        this.draw()
+        this.position.x += this.velocity.vx
+        this.position.y += this.velocity.vy
+    }
+}
+
+class Projectile {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 5
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+        ctx.closePath()
+        ctx.fillStyle = 'white'
+        ctx.fill()
+    }
+
+    update () {
         this.draw()
         this.position.x += this.velocity.vx
         this.position.y += this.velocity.vy
@@ -61,6 +83,9 @@ const keys = {
 const SPEED = 3
 const ROTATIONAL_SPEED = 0.05
 const FRICTION = .97
+const PROJECTILE_SPEED = 3
+
+const projectiles = []
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -68,6 +93,17 @@ function animate() {
     ctx.fillRect(0,0,canvas.width,canvas.height)
 
     player.update()
+
+    for(let i = projectiles.length - 1; i >= 0; i--) {
+        const projectile = projectiles[i]
+        projectile.update()
+
+        if (projectile.position.x + projectile.radius < 0 ||
+            projectile.position.x - projectile.radius > canvas.width ||
+            projectile.position.y + projectile.radius < 0 ||
+            projectile.position.y - projectile.radius > canvas.height
+        ) projectiles.splice(i, 1)
+    }
 
     if (keys.w.pressed) {
         player.velocity.vx = Math.cos(player.rotation) * SPEED
@@ -94,6 +130,17 @@ window.addEventListener('keydown', (event) => {
         case 'KeyD':
             keys.d.pressed = true
             break    
+        case 'Space':
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + Math.cos(player.rotation) * 30,
+                    y: player.position.y + Math.sin(player.rotation) * 30
+                },
+                velocity: {
+                    vx: Math.cos(player.rotation) * PROJECTILE_SPEED,
+                    vy: Math.sin(player.rotation) * PROJECTILE_SPEED
+                }
+            }))
     }
 })
 
